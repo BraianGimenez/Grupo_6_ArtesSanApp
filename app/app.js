403e93e2ba2725d210651026f8e3e9e1fa777ac3
Comision19 <cristianbeliera@gmail.com>
2023-01-16 16:08:32 -0300

    Forms Login y Register

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express()
 const path = require("path")
-const PORT = 3000;
+const PORT = 3030;
 
 app.use(express.static("public"));
 
@@ -13,6 +13,13 @@ app.get("/cart", (req, res) => {
 })
 app.get("/productDetail", (req, res) => {
     return res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+})
+app.get("/register", (req, res) => {
+    return res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login", (req, res) => {
+    return res.sendFile(path.join(__dirname, "/views/login.html"))
 })
 
 app.listen(PORT, () => console.log(`servidor levantando en puerto : ${PORT}\n http://localhost:${PORT}`))
