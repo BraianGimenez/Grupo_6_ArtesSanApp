96463adaa48b6fed99802212b766b7f1b44f7e0a
BraianGimenez <brazz3013@gmail.com>
2023-02-02 20:50:06 -0300

    se corrige error de vista

--- app/src/app.js
+++ app/src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3030;
 
+//Template engines
+app.set("view engine","ejs")
+app.set("views", "./src/views")
+
 // public
 app.use(express.static("public"));
 
@@ -18,7 +22,6 @@ app.use("/product", productRouter);
 app.use("/user", usersRouter);
 
 // ejs engine
-
 
 app.set("view engine", "ejs");
 
